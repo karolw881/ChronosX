e8aba8cc03c5d2e9ee91c5eee56d5747d6022010
karol <karolw881@gmail.com>
2025-03-16 15:20:52 +0100

    14th  still implemnting VectoPerformaceGeneric , add better writer to file, and new operations for vector

--- src/main/java/org/magister/vector/VectorReflectionUtil.java
+++ src/main/java/org/magister/vector/VectorReflectionUtil.java
@@ -21,6 +21,16 @@ public class VectorReflectionUtil {
      * @param <T>           typ elementów wektora
      * @return wynik operacji jako nowy wektor
      */
+
+
+    /**W tym przypadku castowanie jest potrzebne ze względu na sposób działania refleksji w Javie. Chociaż typy generyczne zostały zaprojektowane, aby unikać castowania w normalnym kodzie, refleksja wprowadza pewne komplikacje. Wyjaśnię, dlaczego tak się dzieje:
+     Główny powód to wymazywanie typów (type erasure) w Javie. Podczas kompilacji informacje o typach generycznych są usuwane, więc w czasie wykonania metoda invoke() nie ma informacji o typie generycznym. Dlatego:
+
+     Metoda invoke() z klasy Method zwraca obiekt typu Object, niezależnie od rzeczywistego typu zwracanego przez wywoływaną metodę.
+     W czasie wykonania JVM nie ma informacji o typie generycznym T, więc nie może automatycznie dokonać konwersji z Object na Vector<T>.
+     Programista musi jawnie poinformować kompilator poprzez castowanie, że wynik wywołania metody powinien być traktowany jako Vector<T>.
+
+     */
     public static <T extends Number > Vector<T> performOperationReflectVector(Vector<T> vector1, Vector<T> vector2, String operationName) {
         try {
             Method method = Vector.class.getMethod(operationName, Vector.class);
@@ -31,6 +41,43 @@ public class VectorReflectionUtil {
                     "' na wektorach za pomocą refleksji: " + e.getMessage(), e);
         }
     }
+
+    public static <T extends Number > int performOperationReflectVectorForDotProduct(Vector<T> vector1, Vector<T> vector2, String operationName) {
+        try {
+            Method method = Vector.class.getMethod(operationName, Vector.class);
+            int result = (int) method.invoke(vector1, vector2);
+            return result;
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException("Błąd podczas wykonywania operacji '" + operationName +
+                    "' na wektorach za pomocą refleksji: " + e.getMessage(), e);
+        }
+    }
+
+
+    public static <T extends Number> Vector<T> performOperationReflectVectorForScalar(Vector<T> vector, Number scalar, String operationName) {
+        try {
+            Method method = Vector.class.getMethod(operationName, Number.class);
+            Vector<T> result = (Vector<T>) method.invoke(vector, scalar);
+            return result;
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException("Błąd podczas wykonywania operacji '" + operationName +
+                    "' na wektorze za pomocą refleksji: " + e.getMessage(), e);
+        }
+    }
+
+    public static <T extends Number> Vector<T> performOperationReflectVectorForOpposite(Vector<T> vector,  String operationName) {
+        try {
+            Method method = Vector.class.getMethod(operationName);
+            Vector<T> result = (Vector<T>) method.invoke(vector);
+            return result;
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException("Błąd podczas wykonywania operacji '" + operationName +
+                    "' na wektorze za pomocą refleksji: " + e.getMessage(), e);
+        }
+    }
+
+
+
 
 
     public static Vector1 performOperationReflectVector1(Vector1 vector1, Vector1 vector2, String operation){
